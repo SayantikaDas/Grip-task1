7dec1dac140e8d5aa9e20e96dca87bccca039743
Sayantika Das <76109832+SayantikaDas@users.noreply.github.com>
2020-12-16 19:18:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Shashi Kumar D M">
+    <meta name="copy rights" content="SayantikaDas@20">
+    <title>Home Page | Sayantika Das</title>
+
+    <!-- styles -->
+    <link rel="stylesheet" href="./assets/css/main.min.css">
+
+</head>
+<body>
+
+    <?php include "./templets/header.html" ?>
+    
+    <main>
+        <a href="./allCustomers.php" class="view-btn">View all Customers</a>
+    </main>
+
+</body>
+</html>
